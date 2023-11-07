3fd0f86c1b3dead2271bdb741555ab4b32f3e060
sakthivel612 <sv69811@gmail.com>
2023-11-06 16:29:38 -0800

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import TextEditor from './components/TextEditor'
+import { v4 as uuidv4 } from 'uuid';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <Routes>
+        <Route path="/" exact element={<Navigate to={`/document/${uuidv4()}`} />}>
+        </Route>
+        <Route path="/document/:id" Component={TextEditor}></Route>
+      </Routes>
+    </Router>
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/TextEditor.js
@@ -0,0 +1,106 @@
+import React, { useCallback, useEffect, useState } from "react";
+import Quill from "quill";
+import "quill/dist/quill.snow.css";
+import "./TextEditor.css";
+import { io } from "socket.io-client";
+import { useParams } from "react-router-dom";
+
+const Toolbar_Options = [
+  ["bold", "italic", "underline", "strike"], // toggled buttons
+  ["image", "blockquote", "code-block"],
+
+  [{ header: 1 }, { header: 2 }], // custom button values
+  [{ list: "ordered" }, { list: "bullet" }],
+  [{ script: "sub" }, { script: "super" }], // superscript/subscript
+  [{ indent: "-1" }, { indent: "+1" }], // outdent/indent
+  [{ direction: "rtl" }], // text direction
+
+  [{ size: ["small", false, "large", "huge"] }], // custom dropdown
+  [{ header: [1, 2, 3, 4, 5, 6, false] }],
+
+  [{ color: [] }, { background: [] }], // dropdown with defaults from theme
+  [{ font: [] }],
+  [{ align: [] }],
+
+  ["clean"], // remove formatting button
+];
+
+const SAVE_INTERVAL_MS = 2000;
+
+const TextEditor = () => {
+  const [socket, setSocket] = useState();
+  const [quill, setQuill] = useState();
+  const { id: documentId } = useParams();
+
+  useEffect(() => {
+    const s = io("https://project1-backend-3fd8.onrender.com"); // s -> socket
+    setSocket(s);
+    // s.emit("data", "Hey i am connected");
+    return () => {
+      s.disconnect();
+    };
+  }, []);
+
+  useEffect(() => {
+    if (socket == null || quill == null) return;
+    socket.once("load-document", (document) => {
+      console.log(document);
+      quill.setContents(document);
+      quill.enable();
+    });
+    socket.emit("get-document", documentId);
+  }, [socket, quill, documentId]);
+
+  useEffect(() => {
+    if (socket == null || quill == null) return;
+    const handler = (delta, oldDelta, source) => {
+      if (source !== "user") return;
+      socket.emit("send-changes", delta);
+    };
+    quill.on("text-change", handler);
+    return () => {
+      quill.off("text-change", handler);
+    };
+  }, [socket, quill]);
+
+  useEffect(() => {
+    if (socket == null || quill == null) return;
+    const handler = (delta) => {
+      // console.log(delta);
+      quill.updateContents(delta);
+    };
+    socket.on("receive-changes", handler);
+    return () => {
+      socket.off("receive-changes", handler);
+    };
+  }, [socket, quill]);
+
+  useEffect(() => {
+    if (socket == null || quill == null) return;
+    const interval = setInterval(() => {
+      socket.emit("save-document", quill.getContents());
+    }, SAVE_INTERVAL_MS);
+    return () => {
+      clearInterval(interval);
+    };
+  }, [socket, quill]);
+
+  const wrapperRef = useCallback((wrapper) => {
+    if (wrapper == null) return;
+    wrapper.innerHTML = "";
+    const editor = document.createElement("div");
+    wrapper.append(editor);
+    const q = new Quill(editor, {
+      theme: "snow",
+      modules: {
+        toolbar: Toolbar_Options,
+      },
+    });
+    q.disable();
+    q.setText("Loading....");
+    setQuill(q);
+  }, []);
+  return <div className="container" ref={wrapperRef}></div>;
+};
+
+export default TextEditor;
